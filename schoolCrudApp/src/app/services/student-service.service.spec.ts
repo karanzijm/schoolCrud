f51e9bdb5511929f95790fbb8870ef9ce3cd368b
Karanzi John mary <karanzijm@hotmail.com>
2022-12-23 07:08:21 +0300

    Feat: Added final tests

--- schoolCrudApp/src/app/services/student-service.service.spec.ts
+++ schoolCrudApp/src/app/services/student-service.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { StudentServiceService } from './student-service.service';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {environment} from "../../environment/environment";
+
+import {Student} from "../Student/student";
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 describe('StudentServiceService', () => {
   let service: StudentServiceService;
@@ -13,8 +16,27 @@ describe('StudentServiceService', () => {
     httpTestingController = TestBed.inject(HttpTestingController)
     service = TestBed.inject(StudentServiceService);
   });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getStudents should return expected data', (done) => {
+    const expectedData: Student[] = [
+      { id:1, name: 'Karanzi John Mary', surname: 'Karanzi' },
+      { id:1, name: 'James Bond', surname: 'Bond' },
+    ];
+
+    service.getStudents().subscribe(data => {
+      expect(data).toEqual(expectedData);
+      done();
+    });
+
+    const testRequest = httpTestingController.expectOne(environment.apiBaseUrl+'/Student/all');
+
+    testRequest.flush(expectedData);
   });
 });
--- schoolCrudApp/src/app/services/teacher-service.service.spec.ts
+++ schoolCrudApp/src/app/services/teacher-service.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TeacherServiceService } from './teacher-service.service';
-
+import {Teacher} from "../Teacher/teacher";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {Student} from "../Student/student";
+import {environment} from "../../environment/environment";
 
 describe('TeacherServiceService', () => {
   let service: TeacherServiceService;
@@ -15,8 +17,28 @@ describe('TeacherServiceService', () => {
     httpTestingController = TestBed.inject(HttpTestingController)
     service = TestBed.inject(TeacherServiceService);
   });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getStudents should return expected data', (done) => {
+    const expectedData: Teacher[] = [
+      { id:1, name: 'Harrison Ford', studentList: 'Karanzi John Mary, Charles Dickens' },
+      { id:1, name: 'Jack Sparrow', studentList: 'Black beard, kracken' },
+    ];
+
+    service.getTeachers().subscribe(data => {
+      expect(data).toEqual(expectedData);
+      done();
+    });
+
+    const testRequest = httpTestingController.expectOne(environment.apiBaseUrl+'/Teacher/all');
+
+    testRequest.flush(expectedData);
   });
 });
