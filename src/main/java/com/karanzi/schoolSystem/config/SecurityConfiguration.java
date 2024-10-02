411bf331488580141d57a202f0caf5a2f67293fe
Karanzi <karanzi@Karanzis-MacBook-Pro.local>
2024-10-02 22:36:45 +0300

    removed security lib

--- src/main/java/com/karanzi/schoolSystem/config/SecurityConfiguration.java
+++ src/main/java/com/karanzi/schoolSystem/config/SecurityConfiguration.java
@@ -7,10 +7,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
     public CorsFilter corsFilter() {
