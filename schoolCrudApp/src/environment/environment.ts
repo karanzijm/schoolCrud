72dfb158c4982dae7bffcc07efc1dd9d205bb38d
Karanzi <karanzi@Karanzis-MacBook-Pro.local>
2024-10-02 22:40:19 +0300

    changed port

--- schoolCrudApp/src/environment/environment.ts
+++ schoolCrudApp/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8097/api'
+  // apiBaseUrl: 'http://localhost:8097/api'
   // apiBaseUrl: 'http://127.0.0.1:8097/api'
-  // apiBaseUrl: 'http://34.46.243.181:8097/api'
+  apiBaseUrl: 'http://34.46.243.181:8097/api'
 }
